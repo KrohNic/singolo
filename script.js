4e12b15f34414d3ac5001031b270c0f15fd29f0e
Kroh Nickolay <375336623761@ya.ru>
2020-03-20 22:37:40 +0300

    feat: add slider interactive actions

--- script.js
+++ script.js
@@ -49,6 +49,70 @@ class NavMenu {
   }
 }
 
+class Slider {
+  constructor() {
+    this.SLIDER_LEFT = document.querySelector(".slider-controls-left");
+    this.SLIDER_RIGHT = document.querySelector(".slider-controls-right");
+    this.SLIDES_LIST = document.querySelector(".slider--content").children;
+
+    this.isSliderRunning = false;
+    this.curIndex = 0;
+
+    for (let i = 0; i < this.SLIDES_LIST.length; ++i)
+      if (!this.SLIDES_LIST[i].classList.contains("slide--hidden")) {
+        this.curIndex = i;
+        break;
+      }
+
+    this.SLIDER_LEFT.addEventListener("click", () => this.nextSlide(false));
+    this.SLIDER_RIGHT.addEventListener("click", () => this.nextSlide(true));
+
+    for (let i = 0; i < this.SLIDES_LIST.length; ++i)
+      this.SLIDES_LIST[i].addEventListener("animationend", () =>
+        this.animationEndListener(i)
+      );
+  }
+
+  animationEndListener(i) {
+    this.SLIDES_LIST[i].classList.remove("from_right");
+    this.SLIDES_LIST[i].classList.remove("to_left");
+    this.SLIDES_LIST[i].classList.remove("from_left");
+    this.SLIDES_LIST[i].classList.remove("to_right");
+
+    if (this.curIndex !== i) this.SLIDES_LIST[i].classList.add("slide--hidden");
+
+    this.isSliderRunning = false;
+  }
+
+  nextSlide(rightDirection) {
+    if (this.isSliderRunning) return;
+
+    console.log("slide", this);
+
+    this.isSliderRunning = true;
+
+    let nextIndex;
+
+    if (rightDirection) {
+      nextIndex = (this.curIndex + 1) % this.SLIDES_LIST.length;
+
+      this.SLIDES_LIST[nextIndex].classList.remove("slide--hidden");
+      this.SLIDES_LIST[nextIndex].classList.add("from_right");
+      this.SLIDES_LIST[this.curIndex].classList.add("to_left");
+    } else {
+      nextIndex =
+        (this.SLIDES_LIST.length + this.curIndex - 1) % this.SLIDES_LIST.length;
+
+      this.SLIDES_LIST[nextIndex].classList.remove("slide--hidden");
+      this.SLIDES_LIST[nextIndex].classList.add("from_left");
+      this.SLIDES_LIST[this.curIndex].classList.add("to_right");
+    }
+
+    this.SLIDES_LIST[this.curIndex].classList.add("slide--hidden");
+    this.curIndex = nextIndex;
+  }
+}
+
 document.querySelectorAll(".phone--button").forEach(phoneBtn => {
   phoneBtn.addEventListener("click", event => {
     event.target.previousElementSibling.classList.toggle("phone--screen-off");
@@ -56,3 +120,4 @@ document.querySelectorAll(".phone--button").forEach(phoneBtn => {
 });
 
 new NavMenu();
+new Slider();
